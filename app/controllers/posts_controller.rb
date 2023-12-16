45994a557abc770612714443f44172a167e97d0c
Okaayumi <syouma009ich@gmail.com>
2023-12-16 22:40:40 +0900

    #{新規追記まで作成}

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @posts=Post.all
+    @date1 = Date.current.strftime('%Y/%m/%d')
+    @count =Post.count
   end
   def new
+    @post=Post.new
   end
 
   def create
+    @post=Post.new(params.require(:post).permit(:title,:start,:finish,:allday,:note))
+    if @post.save
+      redirect_to:posts
+    else render "new"
+    end
   end
 
   def show
